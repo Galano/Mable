6fc14301dba1e87865a33dd3d772752904a96b1e
Galano <brunogalano@gmail.com>
2018-04-12 19:37:40 -0300

    teste

--- Fonte/index.js
+++ Fonte/index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
